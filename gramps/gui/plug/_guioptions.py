8fcb62f1ee1498b9ef0a177c719424a6b9ec5656
Steve Youngs <steve@youngs.cc>
2026-05-04 18:05:40 +0100

    specify the required Gtk version
    this revents a runtime warning
    /gramps/gui/plug/_guioptions.py:44: PyGIWarning: Gtk was imported without specifying a version first. Use gi.require_version('Gtk', '3.0') before import to ensure that the right version gets loaded.

--- gramps/gui/plug/_guioptions.py
+++ gramps/gui/plug/_guioptions.py
@@ -39,6 +39,9 @@ import os
 # gtk modules
 #
 # -------------------------------------------------------------------------
+import gi
+
+gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import GObject
